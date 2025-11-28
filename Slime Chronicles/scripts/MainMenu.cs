84d2db5b56247cabc8113e2417355fe100a0c93f
TJeffrey237 <tyler_tj@icloud.com>
2025-11-28 11:37:43 -0700

    Added a working version of the title screen.

---
+++ Slime Chronicles/scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public partial class MainMenu : Control
+{
+	// can just change the scene here if we want it to go somewhere else
+	private const string LevelOnePath = "res://scenes/levels/level_one.tscn";
+	// moves to the level one scene
+	private void _on_start_button_pressed()	
+	{
+		if (GetTree() != null)
+		{
+			GetTree().ChangeSceneToFile(LevelOnePath);
+		}
+	}
+	
+	// quits the game when the exit button is pressed
+	private void _on_exit_button_pressed()
+	{
+		if (GetTree() != null)
+		{
+			GetTree().Quit();
+		}
+	}
+}
